f1fbce93969c5d5c5a528bf9839505306194fc2b
Gabriel Stoffel <gc.stoffel03@gmail.com>
2023-03-16 20:34:27 -0300

    user registration

--- controllers/validate.js
+++ controllers/validate.js
@@ -1,7 +1,7 @@
 import joi from '@hapi/joi';
 const registerValidate = (data) => {
     const schema = joi.object({
-        nome: joi.string().required().min(3).max(50),
+        name: joi.string().required().min(3).max(50),
         email: joi.string().required(),
         password: joi.string().min(8).max(100)
     })
--- public/publicViews/script.js
+++ public/publicViews/script.js
@@ -6,22 +6,26 @@ window.addEventListener('DOMContentLoaded', (event) =>{
         let password = document.getElementById('password').value;
         let email = document.getElementById('email').value;
         let registerInformarion = {
-            nome: userName,
+            name: userName,
             email: email,
-            password: password,
+            password: password
         }
         let options = {
             method: "POST",
             headers: new Headers({'content-type': 'application/json'}),
             body: JSON.stringify(registerInformarion),
         }
-        fetch('http://localhost:3030/user/register', options).then(
-            (res) => {
+        console.log(JSON.stringify(registerInformarion));
+        try {
+            fetch('http://localhost:3030/user/register', options).then(
+                (res) => {
                 console.log(res);
                 document.getElementById('userName').value = '';
                 document.getElementById('password').value ='';
                 document.getElementById('email').value ='';
-            },
-        )
+            })
+        } catch (err){
+            console.log(err)
+        }
     })
 })
